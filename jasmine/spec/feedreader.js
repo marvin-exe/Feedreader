aaf8a54b4068e2a8a9f8b7973c8aef7b69f4b27e
marvinduarte <mduarte1894@gmail.com>
2019-09-02 17:18:12 -0400

    add test suites
    add text thatloops trough url/name feeds, add "the menu" test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,32 +27,57 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+         it('should have feed url defined and not empty', function(){
+           for(var i=0, len=allFeeds.length; i<len; i++){
+             expect(allFeeds[i].name).toBeDefined();
+             expect(allFeeds[i].name.length).not.toBe(0);
+           }
+         })
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('should have name defined and not empty', function(){
+           for(var i=0, len=allFeeds.length; i<len; i++){
+             expect(allFeeds[i].name).toBeDefined();
+             expect(allFeeds[i].name.length).not.toBe(0);
+           }
+         });
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* Write a new test suite named "The menu" */
+    describe('The menu', function(){
+      let BODY = $('body'), MENU_BUTTON = $('.menu-icon-link');
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
+
+        /* Test that ensures the menu element is
+         * hidden by default.
          */
+         it('should hidemenu by default', function(){
+           expect(BODY.hasClass('menu-hidden')).toBeTruthy();
+         });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* Test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('should show menu when clicking icon and hide it when clicked again', function(){
+            MENU_BUTTON.click();
+            expect(BODY.hasClass('menu-hidden')).toBeFalsy();
+
+            MENU_BUTTON.click();
+            expect(BODY.hasClass('menu-hidden')).toBeTruthy();
+          })
+
+      })
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
